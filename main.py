0ae296f614ef4e48e3d5c2ca77dddb25ebd9afe7
BriffaultUNAL <miahernandezsa10@gmail.com>
2024-12-05 09:16:35 -0500

    multiple retries

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from src.utils import *
 
 if __name__ == "__main__":
 
-    try:
-        agentes_conectados, df_agentes_en_llamada, df_usersv2, df_agentes_en_pausa, df_agentes_programados, df_ultima_gestion = extract_sql()
+    while True:
 
-        load(cards(agentes_conectados, df_agentes_en_llamada, df_usersv2,
-                   df_agentes_en_pausa, df_agentes_programados), "tb_cards")
-        load(details(agentes_conectados, df_agentes_en_llamada, df_usersv2,
-                     df_agentes_en_pausa, df_agentes_programados, df_ultima_gestion), "tb_details")
-    except Exception as e:
-        logging.info(e)
+        try:
+            agentes_conectados, df_agentes_en_llamada, df_usersv2, df_agentes_en_pausa, df_ultima_gestion, df_operation, df_candidates = extract_sql()
+
+            load(cards(agentes_conectados, df_agentes_en_llamada, df_usersv2,
+                       df_agentes_en_pausa), "tb_cards")
+            load(details(agentes_conectados, df_agentes_en_llamada, df_usersv2,
+                         df_agentes_en_pausa, df_ultima_gestion, df_operation, df_candidates), "tb_details")
+            time.sleep(1)
+        except Exception as e:
+            logging.info(e)
