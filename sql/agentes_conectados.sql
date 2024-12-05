c3df8948d8b41d6fd56311b8a83ad2669f4ae040
BriffaultUNAL <miahernandezsa10@gmail.com>
2024-12-05 09:21:40 -0500

    column updated at added, alias changed and ordered by id

--- sql/agentes_conectados.sql
+++ sql/agentes_conectados.sql
@@ -1,6 +1,6 @@
 SELECT 
-        id, rrhh_id, extension, document as Cedula, username as Usuario
+        id, rrhh_id, extension, document as Cedula_usersv2, username as Usuario, updated_at as updated_at_users
     FROM
         `miosv2-phone`.usersv2
     WHERE
-        extension IS NOT NULL
+        extension IS NOT NULL order by id DESC
