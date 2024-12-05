32b18560df7355177a9f4cb5bf392e4e44f3297d
BriffaultUNAL <miahernandezsa10@gmail.com>
2024-12-05 09:23:08 -0500

    columns added and LEFT JOIN calls, campaigns, operations

--- sql/agentes_en_llamada.sql
+++ sql/agentes_en_llamada.sql
@@ -1,7 +1,16 @@
 SELECT 
-    user_id, start
+    calls.user_id,
+    calls.start,
+    operations.name as Campana,
+    campaigns.name as Skill
 FROM
-    `miosv2-phone`.calls
+    `miosv2-phone`.calls calls
+        LEFT JOIN
+    `miosv2-phone`.campaigns campaigns ON calls.campaign_id = campaigns.id
+        LEFT JOIN
+    `miosv2-phone`.operations operations ON campaigns.operation_id = operations.id
 WHERE
     DATE(start) = CURDATE() AND end IS NULL
-        AND user_id IS NOT NULL
+        AND calls.user_id IS NOT NULL
+        AND calls.state_id = 1
+        AND calls.type_id = 1
