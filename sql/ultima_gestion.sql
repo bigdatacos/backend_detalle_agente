5eeab057f6cf45eb687e0d7a2b49e6ab61e852ee
BriffaultUNAL <miahernandezsa10@gmail.com>
2024-12-05 09:26:40 -0500

    end as start and end no null

--- sql/ultima_gestion.sql
+++ sql/ultima_gestion.sql
@@ -1,5 +1,5 @@
 SELECT 
-    t1.user_id, t1.start
+    t1.user_id, t1.end as start
 FROM
     `miosv2-phone`.calls t1
 WHERE
@@ -9,4 +9,5 @@ WHERE
             `miosv2-phone`.calls t2
         WHERE
             t2.user_id = t1.user_id
+                AND t2.end IS NOT NULL
                 AND DATE(t2.start) = CURDATE());
